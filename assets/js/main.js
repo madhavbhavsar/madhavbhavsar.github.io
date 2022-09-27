ad88ae7c651ea6f9daabdb1d0e6f7c6df2f71643
Madhav Bhavsar <madhavbhavsar05@gmail.com>
2022-09-28 02:54:25 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,15 @@ sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
 });
 sr.reveal(".home__social-icon", { interval: 200 });
 sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
+
+function copy() {
+  var Url = document.getElementById("box");
+  Url.value = "madhavbhavsar05@gmail.com";
+  Url.focus();
+  Url.select();
+  document.getElementById("custom-tooltip").style.display = "inline";
+  document.execCommand("copy");
+  setTimeout(function () {
+    document.getElementById("custom-tooltip").style.display = "none";
+  }, 1000);
+}
